d50129f92cdb1107c46140012bd04951681b0069
Ben Y. <bjy777101@gmail.com>
2026-03-01 11:24:24 +1100

    [FEAT] : Split interface to make it easier to read, added rpi build
    Split the interface into;
    - `default.nix` : Config entrypoint
    - `interface.nix` : Loader for pins, etc.

--- default.nix
+++ default.nix
@@ -1,93 +1,7 @@
 let
-    sources = import ./npins;
-
-    # --- Standard ---
-    pkgs = import sources.nixpkgs {};
-    lib = pkgs.lib;
-
-    # --- Internal ---
-    # common exposes a custom nix library + out of tree packages  + lists of common modules
-    # common modules are classified as;
-    # - data : only used as a wrapper for representing some data (compatible with nixos and home manager)
-    # - nixos : generates nixos config, compatible only with nixos
-    # - home : generates home manager config, compatible only with home manager
-    common = import sources.common;
-    extlib = common.extlib { inherit sources; inherit pkgs; inherit lib; };
-    common-extpkgs = common.extpkgs { inherit pkgs; };
-    commonDataModules = common.data;
-    commonNixosModules = common.nixos;
-    commonHomeModules = common.home;
-
-    # hosts and users provide an attrset of <name> -> list of dedicated modules
-    # (hosts being nixos configs, users being home-manager configs)
-    # users also provides a `userModules` attribute that exposes generic users
-    hosts = import sources.hosts;
-    users = import sources.users;
-
-    # secrets is an interface providing;
-    # - data : secret realisations of data modules
-    # - ageSecretFiles : an attrset of <name> -> <age secret files>
-    #                    forwarded to modules as `secrets` argument
-    secrets = import sources.secrets;
-
-    # --- External ---
-    flake-compat = import sources.flake-compat;
-    home-manager = import sources.home-manager { inherit pkgs; };
-    nur = import sources.nur {
-        nurpkgs = pkgs;
-        pkgs = import sources.nixpkgs {
-            overrides = [ (final: prev: if prev ? nur then prev else { nur = import sources.nur { pkgs = final; }; }) ];
-        };
-    };
-    rpi = import sources.nixos-rpi;
-    agenix = (flake-compat { src = sources.agenix.outPath; }).defaultNix;
-
-    externalNixosModules = [
-        agenix.nixosModules.age
-    ];
-
-    externalHomeModules = [
-    ];
-
-    # Merge extpkgs from common w/ external packages
-    extpkgs = {
-        # agenix/default.nix results in { agenix = <pkg drv>; ... }
-        agenix = (import sources.agenix { inherit pkgs; }).agenix;
-        inherit (home-manager) home-manager;
-        # Not a package, but it makes the most sense
-        # to put it here
-        inherit nur;
-    } // common-extpkgs;
-
-    # Combine common/secret internal and external modules
-    dataModules = commonDataModules ++ secrets.data;
-    nixosModules = commonNixosModules ++ externalNixosModules;
-    homeModules = commonHomeModules ++ externalHomeModules;
-
-    # --- Args ---
-    args = {
-        inherit sources;
-        inherit extlib;
-        inherit extpkgs;
-        secrets = secrets.ageSecretFiles;
-    };
-
-    # FUTURE : Currently, there is no difference in arguments
-    nixosArgs = args;
-    homeArgs = args;
-
-    mkNixos = hostModule: hostUsers: extlib.makeHost (
-        [ hostModule ] ++
-        hostUsers      ++
-        dataModules    ++
-        nixosModules
-    ) nixosArgs;
-
-    mkUser = userModule: extlib.makeHome (
-        [ userModule ] ++
-        dataModules    ++
-        homeModules
-    ) homeArgs;
+    interface = import ./interface.nix;
+    inherit (interface) hosts users;
+    inherit (interface.eval) mkNixos mkNixosPi mkUser;
 in {
     nixpad = mkNixos hosts.nixpad [
         (users.userModules.namedMainUser "ben")
@@ -103,13 +17,11 @@ in {
         (users.userModules.namedNodeUser "node")
     ];
 
-    # TODO : May have to be a special host function
-    # that uses nixos-raspberrypi
-    # nixpi = TODO ... [
-    #    users.userModules.admin
-    #    users.userModules.staging
-    #    users.userModules.node
-    # ];
+    nixpi = mkNixosPi hosts.nixpi [
+       users.userModules.admin
+       users.userModules.staging
+       (users.userModules.namedNodeUser "node")
+    ];
 
     ben = mkUser users.ben;
 }
---
+++ interface.nix
@@ -0,0 +1,144 @@
+/*
+    Handles loading all pins
+*/
+let
+    sources = import ./npins;
+
+    # --- Standard ---
+    pkgs = import sources.nixpkgs { };
+    lib = pkgs.lib;
+
+
+    # --- Internal ---
+    # common exposes a custom nix library + out of tree packages  + lists of common modules
+    # common modules are classified as;
+    # - data : only used as a wrapper for representing some data (compatible with nixos and home manager)
+    # - nixos : generates nixos config, compatible only with nixos
+    # - home : generates home manager config, compatible only with home manager
+    common = import sources.common;
+    extlib = common.extlib { inherit sources; inherit pkgs; inherit lib; };
+    commonExtpkgs = common.extpkgs { inherit pkgs; };
+    commonDataModules = common.data;
+    commonNixosModules = common.nixos;
+    commonHomeModules = common.home;
+
+    # hosts and users provide an attrset of <name> -> list of dedicated modules
+    # (hosts being nixos configs, users being home-manager configs)
+    # users also provides a `userModules` attribute that exposes generic users
+    hosts = import sources.hosts;
+    users = import sources.users;
+
+    # secrets is an interface providing;
+    # - data : secret realisations of data modules
+    # - ageSecretFiles : an attrset of <name> -> <age secret files>
+    #                    forwarded to modules as `secrets` argument
+    secrets = import sources.secrets;
+
+
+    # --- External ---
+    flake-compat = import sources.flake-compat;
+
+    # my fork of home-manager (changes `home-manager` script to support pre-eval)
+    home-manager = import sources.home-manager { inherit pkgs; };
+
+    nur = import sources.nur {
+        nurpkgs = pkgs;
+        pkgs = import sources.nixpkgs {
+            overrides = [ (final: prev: if prev ? nur then prev else { nur = import sources.nur { pkgs = final; }; }) ];
+        };
+    };
+
+    rpi = import sources.nixos-rpi;
+    # modules exposed by nixos-rpi required for a pi build
+    rpiModules = [
+        rpi.lib.inject-overlays
+        rpi.lib.inject-overlays-global
+        rpi.nixosModules.trusted-nix-caches
+        rpi.nixosModules.nixpkgs-rpi
+    ];
+
+    agenix = (flake-compat { src = sources.agenix.outPath; }).defaultNix;
+
+
+    # --- Bundle Components ---
+    externalNixosModules = [
+        agenix.nixosModules.age
+    ];
+
+    externalHomeModules = [
+    ];
+
+    # Combine common/secret internal and external modules
+    dataModules = commonDataModules ++ secrets.data;
+    nixosModules = commonNixosModules ++ externalNixosModules;
+    homeModules = commonHomeModules ++ externalHomeModules;
+
+    # Merge extpkgs from common w/ external packages
+    extpkgs = {
+        # agenix/default.nix results in { agenix = <pkg drv>; ... }
+        agenix = (import sources.agenix { inherit pkgs; }).agenix;
+        inherit (home-manager) home-manager;
+        # Not a package, but it makes the most sense
+        # to put it here
+        inherit nur;
+    } // commonExtpkgs;
+
+
+    # --- Args ---
+    args = {
+        inherit sources;
+        inherit extlib;
+        inherit extpkgs;
+        secrets = secrets.ageSecretFiles;
+    };
+
+    # FUTURE : Currently, there is no difference in arguments
+    nixosArgs = args;
+    homeArgs = args;
+
+
+    # --- Eval Wrapper Functions ---
+    mkNixos = hostModule: hostUsers: extlib.makeHost (
+        [ hostModule ] ++
+        hostUsers      ++
+        dataModules    ++
+        nixosModules
+    ) nixosArgs;
+
+    mkNixosPi = hostModule: hostUsers: extlib.makeHost (
+        [ hostModule ] ++
+        hostUsers      ++
+        dataModules    ++
+        nixosModules   ++
+        rpiModules
+    ) (nixosArgs // { nixos-raspberrypi = rpi; });
+
+    mkUser = userModule: extlib.makeHome (
+        [ userModule ] ++
+        dataModules    ++
+        homeModules
+    ) homeArgs;
+in {
+    # --- Interface ---
+    # Expose actual hosts and users
+    inherit hosts users;
+
+    # Expose config eval functions
+    eval = {
+        inherit mkNixos mkNixosPi;
+        inherit mkUser;
+    };
+
+    # --- Debug ---
+    # Expose pin sources
+    inherit sources;
+
+    # Expose instantiated pins
+    pins = {
+        inherit pkgs lib;
+        inherit common hosts users secrets;
+        inherit flake-compat home-manager nur rpi agenix;
+        # Technically not true pins, but nice to expose regardless
+        inherit extlib extpkgs;
+    };
+}
